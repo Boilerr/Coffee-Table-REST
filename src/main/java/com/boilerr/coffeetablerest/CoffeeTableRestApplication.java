9fd4971c4a8b9c8c16698181957bcdeaab76536d
Boilerr <sergey.nys@gmail.com>
2020-08-18 10:58:02 +0300

    initial commit
    1. https://start.spring.io/
    2. Dependencies: Spring Web
    3. Project Metadata:
    	Group com.boilerr
    	Artifact coffee-table-rest
    	Name coffee-table-rest
    	Description Coffee-Table-REST
    	Package name com.boilerr.coffee-table-rest
    	Packaging Jar
    	Java 8

---
+++ src/main/java/com/boilerr/coffeetablerest/CoffeeTableRestApplication.java
@@ -0,0 +1,13 @@
+package com.boilerr.coffeetablerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeTableRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeTableRestApplication.class, args);
+	}
+
+}
